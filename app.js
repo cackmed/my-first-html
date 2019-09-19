157639783d521d60bc204563b51b2ef9dc170749
Caleb Pendergraft <cackmed@gmail.com>
2019-09-18 21:02:17 -0700

    changed everything

---
+++ app.js
@@ -0,0 +1,21 @@
+alert('hello annoyed one');
+const fruit = ['carrot', 'apple', 'fruit']
+console.log(fruit)
+
+
+for (let i = 0; i <= 5; i++) {
+if (i === 2) {
+ console.log(fruit)
+}
+} 
+function showalert() {
+    alert('you clicked the button');
+}
+
+function logHello() {
+console.log('Hello')
+
+}
+
+const dog = ['carrot']
+console.log(dog)
